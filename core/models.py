e39fdf0b5ceadf3cdbb508c3494238c16413d212
savoloon <nugaev.vlad@mail.ru>
2022-03-19 20:45:56 +0300

    #4 Шестое занятие,поработал с базами данных,добавил авторов и вывел их имена в новой колонке.Также создал параметр заказ.

--- core/models.py
+++ core/models.py
@@ -6,8 +6,21 @@ class Author(models.Model):
 
 
 class Book(models.Model):
+    author = models.ForeignKey('core.Author', on_delete=models.CASCADE, null=True, blank=True, related_name='books')
     name = models.CharField('Название', max_length=128)
     pages = models.IntegerField('Количество страниц', blank=True, null=True)
 
+
+    class Meta:
+        ordering = ['pages']
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    date = models.DateField()
+    book = models.ManyToManyField('core.Book')
