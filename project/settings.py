5bd3bea1d7a456af5ead14f2ad1afcb51a1bed61
savoloon <nugaev.vlad@mail.ru>
2022-04-03 03:53:00 +0500

    #19 небольшие изменения

--- project/settings.py
+++ project/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3'),
+if os.getenv('POSTGRESQL_PASS'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'django_test4',
+            'USER': 'django_test4',
+            'PASSWORD': os.getenv('POSTGRESQL_PASS'),
+            'HOST': 'db',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
